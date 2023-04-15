5da08248e65bfe8a7767e59321bcb4365a65aa6d
Merilola <mariahef12@hotmail.com>
2023-04-15 18:46:36 +0000

    first commit

--- src/models.py
+++ src/models.py
@@ -7,6 +7,42 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
+class User(Base):
+    __tablename__ = 'user'
+    id = Column(Integer, primary_key=True)
+    name = Column(String())
+
+class Favorites(Base):
+    __tablename__ = 'favorites'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey("user.id"))
+    basic_data_id = (Integer, ForeignKey("favorites.basic_data_id"))
+
+class Basic_data(Base):
+    __tablename__ = 'basic_data'
+    id = Column(Integer, primary_key=True)
+    name = Column(String())
+    description = Column(String())
+
+class Character(Base):
+    __tablename__ = 'character'
+    id = Column(Integer, primary_key=True)
+    gender = Column(String())
+    hair_color = Column(String())
+    eye_color = Column(String())
+    basic_data_id = Column(Integer, ForeignKey("Character.id"))
+
+class Planet(Base):
+    __tablename__ = 'planet'
+    id = Column(Integer, primary_key=True)
+    population = Column(Integer)
+    terrain = Column(String())
+    climate = Column(String())
+    orbital_period = Column(Integer)
+    rotation_period = Column(Integer)
+    diameter = Column(Integer)
+    basic_data_id = Column(Integer, ForeignKey("planet.id"))
+
 class Person(Base):
     __tablename__ = 'person'
     # Here we define columns for the table person
